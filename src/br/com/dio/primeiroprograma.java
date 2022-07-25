10ce65f1d041ffaac7affe87a1394df05b86cf6d
Alexya <allexya.fortunato@hotmail.com>
2022-07-25 16:24:27 -0300

    corrigindo erro da criação class Gato, e criando nova class Livro

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -5,5 +5,43 @@ public class primeiroprograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println("hello world!!");
+        System.out.println(gato);
+        Livro Livro = new Livro("minha versão de você", 350);
+        System.out.println(Livro);
+
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPáginas;
+
+    public Livro(String nome, Integer numPáginas) {
+        this.nome = nome;
+        this.numPáginas = numPáginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPáginas() {
+        return numPáginas;
+    }
+
+    public void setNumPáginas(Integer numPáginas) {
+        this.numPáginas = numPáginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPáginas=" + numPáginas +
+                '}';
     }
 }
